2e01013215e9097e802b90f5758abb020a7ccaa8
sumitomota <157360454+sumitomota@users.noreply.github.com>
2024-01-23 16:55:56 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,10 +19,13 @@ def favicon():
 @app.route('/hello', methods=['POST'])
 def hello():
    name = request.form.get('name')
-
+   sendForm = request.form.get('SendForm')
+   language = request.form.get('Lang')
+   numberOfTarget = request.form.get('NoOfTarget')
+  
    if name:
        print('Request for hello page received with name=%s' % name)
-       return render_template('hello.html', name = name)
+       return render_template('hello.html', sendForm = sendForm, language=language)
    else:
        print('Request for hello page received with no name or blank name -- redirecting')
        return redirect(url_for('index'))
