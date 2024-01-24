65a08f306a7c8159694783851476828355824f03
sumitomota <157360454+sumitomota@users.noreply.github.com>
2024-01-24 14:34:26 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import os
 
 from flask import (Flask, redirect, render_template, request,
                    send_from_directory, url_for)
-from azure.storage.blob import BlockBlobService, PublicAccess, ContentSettings
-
+# from azure.storage.blob import BlockBlobService, PublicAccess, ContentSettings
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
 app = Flask(__name__)
+
+connect_str = 'DefaultEndpointsProtocol=https;AccountName=storegeaccount1;AccountKey=Zz4fWjzS3VfsGL3GwKc+3LRuPFGEYElzdfZ7npFPbEa/xGudg+p0h1O4EzsqwgPs7khO7QhSxrZ3+AStmys8ZQ==;EndpointSuffix=core.windows.net
 
 account_name = 'cs1100320010dc66396'
 account_key = 'o46GVNrsMLsM/XcPN+SE2CW0wps/IUcKq6awhKJUzZ7hxFpU1scPye0hTFbuV6U+IW3mZ2EmfHSi+AStbZDNfQ=='
@@ -14,7 +16,7 @@ container_name = 'container1'
 # def allowed_file(filename):
 #     return '.' in filename and \
 #           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-    
+  
 @app.route('/')
 def index():
     print('Request for index page received')
@@ -47,6 +49,7 @@ def upload():
         return redirect(request.url)
 
     file = request.files['file']
+
     # If the user does not select a file, the browser submits an
     # empty file without a filename.
     if file.filename == '':
@@ -54,12 +57,18 @@ def upload():
         return redirect(request.url)
     # if file and allowed_file(file.filename):
     if file:
-        block_blob_service = BlockBlobService(account_name=account_name, account_key=account_key)
-        block_blob_service.create_blob_from_stream(
-            container_name,
-            file.filename,
-            file,
-        )
+        # Create the BlobServiceClient object
+        blob_service_client = BlobServiceClient.from_connection_string(connect_str)
+
+        blob_client = blob_service_client.get_blob_client(container='container1', blob=file.filename)
+        blob_client.upload_blob(file, blob_type="BlockBlob")
+
+#        block_blob_service = BlockBlobService(account_name=account_name, account_key=account_key)
+#       block_blob_service.create_blob_from_stream(
+ #           container_name,
+ #           file.filename,
+ #           file,
+ #       )
         return redirect(url_for('finishRequest'))
 
 if __name__ == '__main__':
